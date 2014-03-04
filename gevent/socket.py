888cc3be116987e8962fc56c028b249ec5756321
Denis Bilenko <denis.bilenko@gmail.com>
2014-03-04 14:41:16 +0100

    update socket.py

--- gevent/socket.py
+++ gevent/socket.py
@@ -1,8 +1,8 @@
 import sys
 
 if sys.version_info[0] >= 3:
-    from gevent import socket2 as _source
-else:
     from gevent import socket3 as _source
+else:
+    from gevent import socket2 as _source
 
 globals().update(_source.__dict__)
